9288e5491ca0db3486786fb2045259c10c7f62ba
Peter Skipper <peter.skipper@gmail.com>
2014-09-25 14:01:08 -0700

    Initial classes and start of test file

--- bicycle.py
+++ bicycle.py
@@ -13,9 +13,12 @@ class BikeShop(object):
     def __init__(self, name):
         self.name = name
         self.inventory = {}
+        self.pricelist = {}
         self.profit = 0
     def add_bike(self, bike):
         self.inventory[bike.model] += 1
+    def add_price(self, bike):
+        self.pricelist[bike.model] = bike.expense + markup * bike.expense
     def sell_bike(self, bike):
         if self.inventory[bike.model] < 1:
             print "You have no more in stock!"
@@ -33,4 +36,14 @@ class Customer(object):
     def __init__(self, name, fund):
         self.name = name
         self.fund = fund
-    
+    def find_available(self, bikeshop):
+        print self.name + "can afford the following bikes at " + bikeshop.name +":\n"
+        for model, price in bikeshop.pricelist.iteritems():
+            if price <= self.fund:
+                print model + ": " + str(price)
+    def buy_bike(self, bikeshop, bike):
+        if self.fund >= bikeshop.pricelist[bike.model]:
+            self.fund -= bikeshop.pricelist[bike.model]
+            bikeshop.sell_bike(bike)
+            print self.name + " purchased a " + bike.model + " for $" + str(bikeshop.pricelist[bike.model])
+            print self.name + "now has $" + str(self.fund) + " left."
